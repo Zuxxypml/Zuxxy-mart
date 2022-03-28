738ed6dd0dd2b56ee9e2777cadebb8bdef5d33e0
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-03-28 15:36:22 +0100

    First release and first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Server started on port 8080");
+});
